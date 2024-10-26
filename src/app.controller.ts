ef6043465dd74110672791381f85d5719e2107d3
Susie Douang <soutsada.d@gmail.com>
2024-10-26 16:59:20 -0500

    add NestJS Post and initial TypeORM for Postgres

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +6,14 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getNames() {
+    return await this.appService.getNames();
+  }
+
+  @Post('/name')
+  async addName(@Body('name') name: string) {
+    this.appService.addName(name);
+    console.log('RESPONSE:', name);
+    return name;
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,10 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
+  async addName(name: string) {
+    return { name };
+  }
+  getNames(): string {
     return 'Hello World!';
   }
 }
